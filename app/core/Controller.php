b7ffd661c58d680f7c4c6c28643236a06b4cad82
Nguyen Khang Duy <nguyenkhangduy74@gmail.com>
2026-06-02 13:12:43 +0700

    feat<UI>: add layoutmaster, Partial

--- app/core/Controller.php
+++ app/core/Controller.php
@@ -1,14 +1,13 @@
 <?php
+class Controller{
 
-    class Controller{
-
-        public function model($model){
-            require_once '../app/models/' . $model . '.php';
-            return new $model();
-        }
-        public function view($viewName, $data = []){
-            extract($data);
-            require_once '../app/views/' . $viewName . '.php';
-        }
+    public function model($model){
+        require_once '../app/models/' . $model . '.php';
+        return new $model();
     }
+    public function view($viewName, $data = []){
+        extract($data);
+        require_once '../app/views/' . $viewName . '.php';
+    }
+}
 ?>
